f34aa47100d4e98b1cf029d36008c2e760273365
DevanshSharmaT_T <devansh.sharma@infotech.works>
2026-06-08 17:02:06 +0530

    Fix

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
   config.cloud_name = "dzraqjvp9"
-  config.api_key    = Rails.application.credentials.cloudinary[:api_key]
-  config.api_secret = Rails.application.credentials.cloudinary[:api_secret]
+  config.api_key    = Rails.application.credentials.dig(:cloudinary, :api_key)
+  config.api_secret = Rails.application.credentials.dig(:cloudinary, :api_secret)
   config.secure     = true
 end
